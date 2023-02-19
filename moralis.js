da0d8275cf6f17578f1c47f33b09370b284dda4d
jenish064 <82806117+jenish064@users.noreply.github.com>
2023-02-19 22:56:46 +0530

    Update moralis.js

--- moralis.js
+++ moralis.js
@@ -7,29 +7,37 @@ const Moralis = require("moralis").default;
 
 const App = () => {
   const [state, setState] = useState({
-    data: [],
+    maxArrayLen: 0,
     flag: true,
     dataHash: null,
+    uploadArray: [
+      {
+        path: "myJson.json",
+        content: ["helloworld"],
+      },
+    ],
   });
 
-  let uploadArray = [
-    {
-      path: "myJson.json",
-      content: ["helloworld"],
-    },
-  ];
+  // let tempArray = [];
+  // let uploadArray = [
+  //   {
+  //     path: "myJson.json",
+  //     content: ["helloworld"],
+  //   },
+  // ];
 
   const reRender = async () => {
+    console.log("entered into reRender!");
     if (!Moralis.Core.isStarted) {
       await Moralis.start({
         apiKey:
           "p1KL1YbNWDDp5dwj3Fe0Bj2faF24D6TzohCpXDs4VwjuhiI50CZj6sg9dza8ABh9",
+        // "GFJ8vWRvbbJKjk3ajmfHDeVyNqF0JXw48TzB0QcyTL4YT1Mi4jwfo8FvRXdIa6XP",
       });
     }
 
-    console.log('abi:::', uploadArray)
     const response = await Moralis.EvmApi.ipfs.uploadFolder({
-      abi: uploadArray,
+      abi: state.uploadArray,
     });
 
     setState({
@@ -42,15 +50,28 @@ const App = () => {
     const socket = socketIOClient("http://127.0.0.1:4001/");
     socket.on("message", (data) => {
       // console.log(uploadArray[0].content)
-      setState({ data: data })
+      // setState({ data: data });
+      // tempArray.push(data);
+      const tempArray = { ...state.uploadArray };
+      console.log(tempArray[0].content);
+
+      tempArray[0].content.push(data);
+
+      if (
+        tempArray[0].content.length % 10 === 0 &&
+        tempArray[0].content.length === state.maxArrayLen + 10
+      ) {
+        setState({
+          maxArrayLen: tempArray[0].content.length,
+          uploadArray: tempArray,
+        });
+        console.log("tempArray:...:", tempArray);
+        console.log("maxArrLen:...", state.maxArrayLen);
+
+        reRender();
+      }
     });
-    uploadArray[0].content.push(state.data);
-
-
-    reRender();
-
-    console.log("hash: ", state.dataHash);
-  }, [state.flag]);
+  }, [state.dataHash]);
 
   return <div>"this is the data: " {state.dataHash}</div>;
 };
