473d904023297b161d5d3521087cabb3090a3715
jenish064 <82806117+jenish064@users.noreply.github.com>
2023-02-23 12:27:23 +0530

    its working
    successfully got the data and converted into ipfs

--- moralis.js
+++ moralis.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const Moralis = require("moralis").default;
@@ -7,73 +7,56 @@ const Moralis = require("moralis").default;
 
 const App = () => {
   const [state, setState] = useState({
-    maxArrayLen: 0,
     flag: true,
     dataHash: null,
-    uploadArray: [
-      {
-        path: "myJson.json",
-        content: ["helloworld"],
-      },
-    ],
+    dataLen: 0,
   });
 
-  // let tempArray = [];
-  // let uploadArray = [
-  //   {
-  //     path: "myJson.json",
-  //     content: ["helloworld"],
-  //   },
-  // ];
+  let tempArray = [];
+  let uploadArray = [
+    {
+      path: "myJson.json",
+      content: ['helloData'],
+    },
+  ];
 
   const reRender = async () => {
-    console.log("entered into reRender!");
     if (!Moralis.Core.isStarted) {
       await Moralis.start({
         apiKey:
-          "p1KL1YbNWDDp5dwj3Fe0Bj2faF24D6TzohCpXDs4VwjuhiI50CZj6sg9dza8ABh9",
-        // "GFJ8vWRvbbJKjk3ajmfHDeVyNqF0JXw48TzB0QcyTL4YT1Mi4jwfo8FvRXdIa6XP",
+          'GFJ8vWRvbbJKjk3ajmfHDeVyNqF0JXw48TzB0QcyTL4YT1Mi4jwfo8FvRXdIa6XP',
+        // "p1KL1YbNWDDp5dwj3Fe0Bj2faF24D6TzohCpXDs4VwjuhiI50CZj6sg9dza8ABh9",
       });
     }
 
-    const response = await Moralis.EvmApi.ipfs.uploadFolder({
-      abi: state.uploadArray,
-    });
+    console.log('abi:::', uploadArray)
+    if (uploadArray[0].content.length === state.dataLen + 10) {
+      console.log("data length:>", state.dataLen);
+      const response = await Moralis.EvmApi.ipfs.uploadFolder({
+        abi: uploadArray,
+      });
 
-    setState({
-      dataHash: response.result[0].path,
-      flag: !state.flag,
-    });
+      setState({
+        dataHash: response.result[0].path,
+      });
+    }
+
   };
 
-  useEffect(() => {
-    const socket = socketIOClient("http://127.0.0.1:4001/");
-    socket.on("message", (data) => {
-      // console.log(uploadArray[0].content)
-      // setState({ data: data });
-      // tempArray.push(data);
-      const tempArray = { ...state.uploadArray };
-      console.log(tempArray[0].content);
+  const socket = socketIOClient("http://127.0.0.1:4001/");
+  socket.on("message", (data) => {
+    tempArray.push(data)
 
-      tempArray[0].content.push(data);
+    if ((tempArray.length % 10 === 0) && (tempArray.length === state.dataLen + 10)) {
+      uploadArray[0].content = tempArray;
+      console.log(uploadArray[0].content);
+      reRender();
 
-      if (
-        tempArray[0].content.length % 10 === 0 &&
-        tempArray[0].content.length === state.maxArrayLen + 10
-      ) {
-        setState({
-          maxArrayLen: tempArray[0].content.length,
-          uploadArray: tempArray,
-        });
-        console.log("tempArray:...:", tempArray);
-        console.log("maxArrLen:...", state.maxArrayLen);
+      state.dataLen = tempArray.length;
+    }
+  });
 
-        reRender();
-      }
-    });
-  }, [state.dataHash]);
-
-  return <div>"this is the data: " {state.dataHash}</div>;
+  return <div>{state.dataHash}</div>;
 };
 
 export default App;
